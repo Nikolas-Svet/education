c709b73a114eadc9efebee2a1beb92989a1e7c1c
Nikolas-Svet <nikitaswetckin@mail.ru>
2024-10-14 23:06:11 +0700

    add lab4.2

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -13,12 +13,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 
 
 data class ColorData(val colorName: String, val colorHex: Int)
 
-class Adapter(private val context: Context, private val list: ArrayList<ColorData>) :
-    RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(
+    private val context: Context,
+    private val list: ArrayList<ColorData>,
+    private val onCellClick: (String) -> Unit
+) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val colorView: View = itemView.findViewById(R.id.colorView)
@@ -34,6 +38,10 @@ class Adapter(private val context: Context, private val list: ArrayList<ColorDat
         val colorData = list[position]
         holder.colorView.setBackgroundColor(colorData.colorHex)
         holder.colorName.text = colorData.colorName
+
+        holder.itemView.setOnClickListener {
+            onCellClick(colorData.colorName)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -42,11 +50,19 @@ class Adapter(private val context: Context, private val list: ArrayList<ColorDat
 }
 
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Установка отступов для системы
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val recyclerView: RecyclerView = findViewById(R.id.rView)
 
@@ -58,7 +74,11 @@ class MainActivity : AppCompatActivity() {
             ColorData("MAGENTA", Color.MAGENTA)
         )
 
+        val adapter = Adapter(this, colorList) { colorName ->
+            Toast.makeText(this, "IT'S $colorName", Toast.LENGTH_SHORT).show()
+        }
+
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = Adapter(this, colorList)
+        recyclerView.adapter = adapter
     }
 }
